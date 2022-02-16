5f928dee0af1457cf9f6e8eb4858a21a5bf13a4f
Igor Kostenko <igor.kostenko.1999@mail.ru>
2022-02-16 11:39:41 +0300

    Added currency filter and slug to Product

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -20,29 +20,4 @@ class DefaultController extends AbstractController
         $productList = $entityManager->getRepository(Product::class)->findAll();
         return $this->render('main/default/index.html.twig');
     }
-
-    /**
-     * @Route("/edit-product/{id}", methods="GET|post", name="product_edit", requirements={"id"="\d+"})
-     * @Route("/add-product", methods="GET|post", name="product_add")
-     */
-    public function editProduct(Request $request, int $id = null): Response
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        if ($id) {
-            $product = $productList = $entityManager->getRepository(Product::class)->find($id);
-        } else {
-            $product = new Product();
-        }
-        $form = $this->createForm(EditProductFromType::class, $product);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($product);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
-        }
-        return $this->render('main/default/edit_product.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 }
