0afcd6302877931cc0c7c6d7c000fef5dfc9ed34
Igor Kostenko <igor.kostenko.1999@mail.ru>
2022-03-02 13:13:40 +0300

    before webpack

---
+++ src/Utils/ApiPlatform/Extension/FilterProductQueryExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Utils\ApiPlatform\Extension;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Product;
+use Doctrine\ORM\QueryBuilder;
+
+class FilterProductQueryExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        $this->andWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->andWhere($queryBuilder, $resourceClass);
+    }
+
+    private function andWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Product::class !== $resourceClass) {
+            return;
+        }
+
+        $rootAlies = $queryBuilder->getRootAliases()[0];
+
+        $queryBuilder->andWhere(
+            sprintf("%s.isDeleted = '0'", $rootAlies)
+        );
+    }
+}
